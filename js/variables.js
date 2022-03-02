697da884960229e56ff9c819335a300e441a7762
Siva-DL <sivaprasad.digitallync@gmail.com>
2022-03-02 10:35:23 +0530

    Form Template and JS Intro

---
+++ js/variables.js
@@ -0,0 +1,60 @@
+// alert(fruit);
+var fruit = "orange";
+
+// Data Types:
+
+// Primitive Data Types
+// string, numbers, boolean,
+
+// Special Data Types
+// null , undefined,
+
+// Secondary/
+// array, object, function,
+
+("i had 'apple' fruit for breakfast;");
+('i had "apple" fruit for breakfast;');
+
+// String:
+("###$@#@#@");
+("a1a1a1a1a1a1");
+
+// alert("i had " + fruit + " for breakfast;");
+// alert("i had " + fruit + " for breakfast;");
+
+// Numbers
+
+123123;
+12123023423;
+0.23423 - 233;
+Infinity;
+0;
+
+// Boolean
+
+1 == 2; // false
+2 > 3; // false
+3 > 2; // true
+
+// true
+// false
+
+// null  | undefined
+
+// var Name = 'shiva';
+
+var Name; // undefined
+
+// Name = 'Prasad'
+
+// Arthemetic Operators:
+
+console.log(1 + 2); // 3
+console.log(1 * 2); // 2
+console.log(1 - 2); // -1
+console.log(2 / 2); // 1
+
+console.log(10 + 10 + "1"); // 11 - Concatination
+console.log("1" * 1); // 1
+console.log("1" - 1); // 0
+console.log("1" / 1); // 1
